bdb825634e4354af7c02a8572381aabc54f5c128
Della Herli Rahman <della.herli@gmail.com>
2020-03-16 15:59:08 +0700

    Update regression method and position from Top

--- TryOpencv/TryOpencv.cpp
+++ TryOpencv/TryOpencv.cpp
@@ -1,3 +1,4 @@
+//newlebih lurus
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
@@ -9,9 +10,11 @@ using namespace cv;
 using namespace std;
 
 clock_t t1, t2;
-int Row_size, Col_size, nRow[8] = { 1,-1 }, nCol[8] = { 0,0 }, cumValue, yHorizontal, yMax = -9999999, yMin = 9999999, xMax = -9999999, xMin = 9999999;
+int Row_size, Col_size, cumValue, yHorizontal, xNormal, yMax = -9999999, yMin = 9999999, xMax = -9999999, xMin = 9999999, minYLeftbottom = 9999999, maxYLeftTop = -9999999;
 //nRow[8] = { 0,1,1,1,0,-1,-1,-1 }, nCol[8]= { -1,-1,0,1,1,1,0,-1 };
 long hist[200], histW[4000], countCol[1400], botttomLine[2000];
+float A, B;
+int nRow[8] = { 1,-1 }, nCol[8] = { 0,0 }, posYbottom[4000], posYtop[4000];
 Mat HRWP, imgRGB, white, imageBinary;
 vector<int> posWhiteTop[2000];
 //vector<int> posWhiteBottom[2000];
@@ -89,8 +92,9 @@ Mat findWhiteSpace(Mat src) {
 Mat whiteArea(Mat src) {
 	for (int i = yMin; i <= yMax; i++) {
 		for (int j = xMin; j <= xMax; j++) {
-			if ((int)src.at<Vec3b>(i, j)[2] == 255)
+			if ((int)src.at<Vec3b>(i, j)[2] == 255) {
 				src.at<Vec3b>(i, j) = { 0, 255, 255 };
+			}
 		}
 	}
 	return src;
@@ -166,27 +170,50 @@ Mat translation(Mat source) {
 	}*/
 	int idx, dstCol;
 	for (int i = xMin; i <= xMax; i++) {
+		//	cout << endl << "vertikal";
 		dstCol = yHorizontal;
 		idx = 0;
 		for (int j = yHorizontal; j >= yMin; j--) {
-			if (posWhiteTop[i].size() == 0) dst.at<uchar>(j, i) = source.at<uchar>(j, i);
+			if (posWhiteTop[i].size() == 0) {
+				dst.at<uchar>(j, i) = source.at<uchar>(j, i);
+				/*if (imageBinary.at<uchar>(j, i) == 0) dst.at<Vec3b>(j, i) = { 0,0,0 };
+				else dst.at<Vec3b>(j, i) = { 255,255,255 };*/
+			}
 			else if (posWhiteTop[i].size() != 0 && j != posWhiteTop[i][idx]) {
 				dst.at<uchar>(dstCol, i) = source.at<uchar>(j, i);
 				dstCol--;
 			}
-			else if (idx < posWhiteTop[i].size() - 1) idx++;
-			else idx = posWhiteTop[i].size() - 1;
+			else if (idx < posWhiteTop[i].size() - 1) {
+				//		cout << posWhiteTop[i][idx] << " ";
+				//dst.at<Vec3b>(posWhiteTop[i][idx], i) = { 209, 18, 193 };
+				idx++;
+			}
+			//else idx = posWhiteTop[i].size() - 1;
 		}
-		dstCol = yHorizontal;
-		for (int j = yHorizontal; j <= yMax; j++) {
-			if (posWhiteTop[i].size() == 0) dst.at<uchar>(j, i) = source.at<uchar>(j, i);
+		if (idx < posWhiteTop[i].size() - 1) idx++;
+		dstCol = yHorizontal + 1;
+		//	cout << "\nbottom";
+
+		for (int j = yHorizontal + 1; j <= yMax; j++) {
+			if (posWhiteTop[i].size() == 0) {
+				dst.at<uchar>(j, i) = source.at<uchar>(j, i);
+				/*if (imageBinary.at<uchar>(j, i) == 0) dst.at<Vec3b>(j, i) = { 0,0,0 };
+				else dst.at<Vec3b>(j, i) = { 255,255,255 };*/
+			}
 			else if (posWhiteTop[i].size() != 0 && j != posWhiteTop[i][idx]) {
 				dst.at<uchar>(dstCol, i) = source.at<uchar>(j, i);
 				dstCol++;
 			}
-			else if (idx < posWhiteTop[i].size() - 1) idx++;
+			else if (idx < posWhiteTop[i].size() - 1) {
+				//		cout << posWhiteTop[i][idx] << " ";
+				//dst.at<Vec3b>(posWhiteTop[i][idx], i) = { 209, 18, 193 };
+				idx++;
+			}
 			else idx = posWhiteTop[i].size() - 1;
 		}
+		/*for (int j = yMin; j <= yMax; j++) {
+			if (imageBinary.at<uchar>(j, i) == 0) dst.at<Vec3b>(j, i) = { 0,0,0 };
+		}*/
 	}
 	return dst;
 }
@@ -292,7 +319,7 @@ void drawHist(Mat src) {
 
 Mat RunRLSA(Mat src, Mat dst, int value, int key) {
 	int counts, a = src.rows, b = src.cols, jumPix = 0;
-	//vertikal
+	//horizontal
 	if (key == 1) {
 		for (int i = 0; i < b; i++) {
 			counts = 0;
@@ -317,7 +344,7 @@ Mat RunRLSA(Mat src, Mat dst, int value, int key) {
 		}
 	}
 	else if (key == 2) {
-		//horizontal
+		//vertikal
 		for (int i = 0; i < a; i++) {
 			if ((int)src.at<uchar>(i, 0) == 0) counts = 0;
 			for (int j = 0; j < b; j++) {
@@ -359,117 +386,245 @@ Mat RunRLSA(Mat src, Mat dst, int value, int key) {
 	}
 	return dst;
 }
+Mat clear(Mat src) {
+	Mat dst = imageBinary;
+	for (int i = xMin; i <= xMax; i++) {
+		for (int j = yMin; j <= yMax; j++) {
+			if (src.at<Vec3b>(j, i)[0] == 0 && src.at<Vec3b>(j, i)[1] == 255 && src.at<Vec3b>(j, i)[2] == 255)
+				dst.at<uchar>(j, i) = 255;
+		}
+	}
+	return dst;
+}
+void equalizeRegression(Mat src) {
+	long long n, sumX = 0, sumY = 0, sumXY = 0, sumSQRx = 0, sumXsqr = 0;
+	int center = 0, cek, top = 0, bottom = 0;
+	//topside
+	for (int i = xMin + 65; i <= xMax - 3; i++) {
+		cek = 0;
+		for (int j = yMin; j <= yHorizontal; j++) {
+			if (!(src.at<Vec3b>(j, i)[0] == 0 && src.at<Vec3b>(j, i)[1] == 255 && src.at<Vec3b>(j, i)[2] == 255)) {
+				if ((src.at<Vec3b>(j - 1, i)[0] == 0 || src.at<Vec3b>(j - 1, i)[0] == 255) && src.at<Vec3b>(j - 1, i)[1] == 255 && src.at<Vec3b>(j - 1, i)[2] == 255) {
+					bottom = j;
+					cek++;
+				}
+				if ((src.at<Vec3b>(j + 1, i)[0] == 0 || src.at<Vec3b>(j + 1, i)[0] == 255) && src.at<Vec3b>(j + 1, i)[1] == 255 && src.at<Vec3b>(j + 1, i)[2] == 255) {
+					top = j;
+					cek++;
+				}
+				if (cek == 2) {
+					if (top - bottom > 4)
+						center = (top + bottom) / 2;
+					if (posYtop[i - 1] - center > 40 && i > xMin + 65) {
+						cek = 0;
+						continue;
+					}
+					maxYLeftTop = max(maxYLeftTop, center);
+					/*sumX += i;
+					sumY += center;
+					sumXY += (i * center);
+					sumSQRx += (i * i);*/
+					posYtop[i] = center;
+					//	cout << i << " " <<top<<" "<<bottom<<" "<< center << endl;
+					break;
+				}
+			}
+		}
+	}
+	//bottom side
 
-int xRegression(int x, int value) {
-	int sumFirstPixel = ((-value) * (x - (xMin)) / (xMax - (xMin))) + value;
+	for (int i = xMin; i <= xMax - 50; i++) {
+		cek = 0;
+		for (int j = yMax; j > yHorizontal; j--) {
+			if (!(src.at<Vec3b>(j, i)[0] == 0 && src.at<Vec3b>(j, i)[1] == 255 && src.at<Vec3b>(j, i)[2] == 255)) {
+				if (src.at<Vec3b>(j - 1, i)[0] == 0 && src.at<Vec3b>(j - 1, i)[1] == 255 && src.at<Vec3b>(j - 1, i)[2] == 255) {
+					bottom = j;
+					cek++;
+				}
+				else if (src.at<Vec3b>(j + 1, i)[0] == 0 && src.at<Vec3b>(j + 1, i)[1] == 255 && src.at<Vec3b>(j + 1, i)[2] == 255) {
+					top = j;
+					cek++;
+				}
+				if (cek == 2) {
+					center = (top + bottom) / 2;
+
+					minYLeftbottom = min(minYLeftbottom, center);
+					/*sumX += i;
+					sumY += center;
+					sumXY += (i * center);
+					sumSQRx += (i * i);*/
+					posYbottom[i] = center;
+					//	cout << i << " " << top << " " << bottom << " " << center << endl;
+					break;
+				}
+			}
+			else cek = 0;
+		}
+	}
+	/*n = (xMax - 100) - xMin + 1;
+	sumXsqr = sumX * sumX;
+	B = (n * sumXY - (sumX * sumY)) / ((n * sumSQRx) - sumXsqr);
+	A = (sumX / n) - B * (sumY / n);*/
+	cout << "regre " << sumX << " " << sumY << " " << sumXY << " " << sumSQRx << " " << sumXsqr << " " << A << " " << B << " " << maxYLeftTop << endl;
+}
+int xRegression(int x) {
+	//int sumFirstPixel = ((-value) * (x - (xMin)) / ((xMax + 50) - (xMin))) + value;
+	int sumFirstPixel = A + B * x;
 	return sumFirstPixel;
 }
 
 int yRegression(int y, int value) {
 	int sumPixelY;
 	if (y <= yHorizontal) {
-		sumPixelY = value - (value * (y - yMin) / (yHorizontal - yMin));
+		sumPixelY = value - (value * (y - yMin) / ((yHorizontal)-yMin));
 	}
 	else {
-		sumPixelY = (value * (y - yHorizontal) / (yMax - yHorizontal));
+		sumPixelY = (value * (y - (yHorizontal)) / (yMax - (yHorizontal)));
 	}
 	return sumPixelY;
 }
 
 
 void deleteWhitePixel(Mat src, int value) {
-	int histW[2000], top, bottom, center, cek, sumPix, dif;
+	int histW[2000], top, bottom, center, cek, sumPix, dif, count = 0;
 	Mat histoW(50, 2000, CV_8UC3, Scalar(255, 255, 255));
 	bool tes = 0;
+	//int limit = (xMax - xMin) / value;
+	ofstream myfile("output.txt");
+
 	for (int i = xMin; i <= xMax; i++) {
-		cek = 0;
-		cumValue = 0;
-		cout << endl << i << "=";
-		for (int j = yHorizontal; j >= yMin; j--) {
-			if (src.at<Vec3b>(j, i)[0] == 0 && src.at<Vec3b>(j, i)[1] == 255 && src.at<Vec3b>(j, i)[2] == 255) {
-				if (j == yHorizontal) {
-					bottom = j;
-					cek++;
-				}
-				else if (src.at<Vec3b>(j - 1, i)[0] == 255 || src.at<Vec3b>(j - 1, i)[2] == 150 || src.at<Vec3b>(j - 1, i)[2] == 0) {
-					top = j;
-					cek++;
-				}
-				else if (src.at<Vec3b>(j + 1, i)[0] == 255 || src.at<Vec3b>(j + 1, i)[2] == 150 || src.at<Vec3b>(j + 1, i)[2] == 0) {
-					bottom = j;
-					cek++;
-				}
-				if (cek == 2) {
-					center = (bottom + top) / 2;
-					sumPix = yRegression(center, (xRegression(i, value)));
-					if (tes) sumPix++;
-					if (cumValue > sumPix) sumPix = 0;
-					else sumPix -= cumValue;
-					cumValue += sumPix;
-					//if (i == xMax - 30) sumPix += value;;
-					cek = 0;
-					if (sumPix > 0) {
-						for (int k = center + ((sumPix - 1) / 2); k >= center - ((sumPix - 1) / 2); k--) {
-							//if (src.at<Vec3b>(k, i)[0] == 0 && src.at<Vec3b>(k, i)[1] == 255 && src.at<Vec3b>(k, i)[2] == 255)
-							posWhiteTop[i].push_back(k);
-						}
-						//dif = bottom - top - sumPix+1;
-						//for (int k = top + (dif / 2) + sumPix - 1; k >= top + (dif / 2); k--) {
-						//	if (src.at<Vec3b>(k, i)[0] == 0 && src.at<Vec3b>(k, i)[1] == 255 && src.at<Vec3b>(k, i)[2] == 255)  posWhiteTop[i].push_back(k);
-						//	//cout << k << " ";
-						//}
-						/*dif = bottom - top - sumPix + 1;
-						if (dif >0) {
-						for (int k = top + (dif / 2) + sumPix - 1; k >= top + (dif / 2); k--) {
-							if (src.at<Vec3b>(k, i)[0] == 0 && src.at<Vec3b>(k, i)[1] == 255 && src.at<Vec3b>(k, i)[2] == 255)  posWhiteTop[i].push_back(k);
-						}
-						}
-						else {
-							for (int k = bottom; k >= top; k--) {
+		if (myfile.is_open())
+		{
+			myfile << endl << i << " total = ";//<< xRegression(i);
+		/*if (count == limit) {
+			value--;
+			count = 0;
+		}
+		count++;*/
+			cek = 0;
+			cumValue = 0;
+			if (i > xMin + 65 && abs(posYtop[i] - posYtop[i - 1]) > 1) posYtop[i] = (posYtop[i] + posYtop[i - 1]) / 2;
+			for (int j = yHorizontal; j >= yMin; j--) {
+				if (src.at<Vec3b>(j, i)[0] == 0 && src.at<Vec3b>(j, i)[1] == 255 && src.at<Vec3b>(j, i)[2] == 255) {
+					if (j == yHorizontal) {
+						bottom = j;
+						cek++;
+					}
+					else if (src.at<Vec3b>(j + 1, i)[0] == 255 || src.at<Vec3b>(j + 1, i)[2] == 150 || src.at<Vec3b>(j + 1, i)[2] == 0) {
+						bottom = j;
+						cek++;
+					}
+					if (src.at<Vec3b>(j - 1, i)[0] == 255 || src.at<Vec3b>(j - 1, i)[2] == 150 || src.at<Vec3b>(j - 1, i)[2] == 0) {
+						top = j;
+						cek++;
+					}
+					if (cek == 2) {
+						center = (top + bottom) / 2;
+						/*if (bottom-top <= 2) {
+							cek = 0;
+							continue;
+						}*/
+						//sumPix = yRegression(center, (xRegression(i, value)));
+						//value = xRegression(i) - minimum;
+
+						value = maxYLeftTop - posYtop[i];
+						if (i <= xMin + 67) value = maxYLeftTop - posYtop[xMin + 67];
+						else if (i >= xMax - 3) value = 0;
+						//cout << value << " ";
+						sumPix = yRegression(center, value);
+						//cout << center << " ";
+						//if (tes) sumPix++;
+						myfile << sumPix << " ";
+						if (cumValue > sumPix) sumPix = 0;
+						else sumPix -= cumValue;
+						cumValue += sumPix;
+
+						//if (i == xMax - 30) sumPix += value;;
+						cek = 0;
+						if (sumPix > 0) {
+							for (int k = top + sumPix - 1; k >= top; k--) {
+								//if (src.at<Vec3b>(k, i)[0] == 0 && src.at<Vec3b>(k, i)[1] == 255 && src.at<Vec3b>(k, i)[2] == 255)
+								posWhiteTop[i].push_back(k);
+
+							}
+							//for (int k = center + ((sumPix - 1) / 2); k >= center - ((sumPix - 1) / 2); k--) {
+							//	//if (src.at<Vec3b>(k, i)[0] == 0 && src.at<Vec3b>(k, i)[1] == 255 && src.at<Vec3b>(k, i)[2] == 255)
+							//	posWhiteTop[i].push_back(k);
+							//	//if (i <= 344 || i <= 345) cout << k << " ";
+							//}
+							//dif = bottom - top - sumPix+1;
+							//for (int k = top + (dif / 2) + sumPix - 1; k >= top + (dif / 2); k--) {
+							//	if (src.at<Vec3b>(k, i)[0] == 0 && src.at<Vec3b>(k, i)[1] == 255 && src.at<Vec3b>(k, i)[2] == 255)  posWhiteTop[i].push_back(k);
+							//	//cout << k << " ";
+							//}
+							/*dif = bottom - top - sumPix + 1;
+							if (dif >0) {
+							for (int k = top + (dif / 2) + sumPix - 1; k >= top + (dif / 2); k--) {
 								if (src.at<Vec3b>(k, i)[0] == 0 && src.at<Vec3b>(k, i)[1] == 255 && src.at<Vec3b>(k, i)[2] == 255)  posWhiteTop[i].push_back(k);
 							}
-						}*/
-					}
-				}
-			}
-		}
-		cumValue = 0;
-		cek = 0;
-		for (int j = yHorizontal; j <= yMax; j++) {
-			if (src.at<Vec3b>(j, i)[0] == 0 && src.at<Vec3b>(j, i)[1] == 255 && src.at<Vec3b>(j, i)[2] == 255) {
-				if (j == yHorizontal) {
-					top = j;
-					cek++;
-				}
-				else if (src.at<Vec3b>(j - 1, i)[0] == 255 || src.at<Vec3b>(j - 1, i)[2] == 150 || src.at<Vec3b>(j - 1, i)[2] == 0) {
-					top = j;
-					cek++;
-				}
-				else if (src.at<Vec3b>(j + 1, i)[0] == 255 || src.at<Vec3b>(j + 1, i)[2] == 150 || src.at<Vec3b>(j + 1, i)[2] == 0) {
-					bottom = j;
-					cek++;
-				}
-				if (cek == 2) {
-					sumPix = 0;
-					center = (bottom + top) / 2;
-					sumPix = yRegression(center, (xRegression(i, value)));
-					cout << sumPix << " ";
-					if (cumValue > sumPix) sumPix = 0;
-					else sumPix -= cumValue;
-					cumValue += sumPix;
-					cek = 0;
-					if (sumPix > 0) {
-						for (int k = center - ((sumPix - 1) / 2); k <= center + ((sumPix - 1) / 2); k++) {
-							//	if (src.at<Vec3b>(k, i)[0] == 0 && src.at<Vec3b>(k, i)[1] == 255 && src.at<Vec3b>(k, i)[2] == 255) 
-							posWhiteTop[i].push_back(k);
+							}
+							else {
+								for (int k = bottom; k >= top; k--) {
+									if (src.at<Vec3b>(k, i)[0] == 0 && src.at<Vec3b>(k, i)[1] == 255 && src.at<Vec3b>(k, i)[2] == 255)  posWhiteTop[i].push_back(k);
+								}
+							}*/
 						}
 					}
 				}
+				else cek = 0;
+			}
+			//	cout << endl;
+			cumValue = 0;
+			cek = 0;
+			//if (i > xMin&& abs(posYbottom[i] - posYbottom[i - 1]) > 1) posYbottom[i] = (posYbottom[i - 1] + posYbottom[i]) / 2;
+			for (int j = yHorizontal + 1; j <= yMax; j++) {
+				if (src.at<Vec3b>(j, i)[0] == 0 && src.at<Vec3b>(j, i)[1] == 255 && src.at<Vec3b>(j, i)[2] == 255) {
+					if (j == yHorizontal) {
+						top = j;
+						cek++;
+					}
+					else if (src.at<Vec3b>(j - 1, i)[0] == 255 || src.at<Vec3b>(j - 1, i)[2] == 150 || src.at<Vec3b>(j - 1, i)[2] == 0) {
+						top = j;
+						cek++;
+					}
+					if (src.at<Vec3b>(j + 1, i)[0] == 255 || src.at<Vec3b>(j + 1, i)[2] == 150 || src.at<Vec3b>(j + 1, i)[2] == 0) {
+						bottom = j;
+						cek++;
+					}
+					if (cek == 2) {
+						sumPix = 0;
+						center = (bottom + top) / 2;
+						value = posYbottom[i] - minYLeftbottom;
+						if (value < 0) value = 0;
+						//sumPix = yRegression(center, (xRegression(i, value)));
+						sumPix = yRegression(center, value);
+						if (sumPix <= 0) sumPix = 0;
+						//cout << value << " ";
+						//if (i >= 313 && i <= 316) cout << sumPix << " ";
+						if (cumValue > sumPix) sumPix = 0;
+						else sumPix -= cumValue;
+						cumValue += sumPix;
+						cek = 0;
+						if (sumPix > 0) {
+							//for (int k = center - ((sumPix - 1) / 2); k <= center + ((sumPix - 1) / 2); k++) {
+							//	//	if (src.at<Vec3b>(k, i)[0] == 0 && src.at<Vec3b>(k, i)[1] == 255 && src.at<Vec3b>(k, i)[2] == 255) 
+							//	posWhiteTop[i].push_back(k);
+							//	//if(i<=344 || i <= 345) cout << k << " ";
+							//}
+							for (int k = bottom - sumPix + 1; k <= bottom; k++) {
+								//if (src.at<Vec3b>(k, i)[0] == 0 && src.at<Vec3b>(k, i)[1] == 255 && src.at<Vec3b>(k, i)[2] == 255)
+								posWhiteTop[i].push_back(k);
+								//if (i <= 344 || i <= 345) cout << k << " ";
+							}
+						}
+					}
+				}
+				else cek = 0;
 			}
 		}
-
 		histW[i] = posWhiteTop[i].size();// +posWhiteBottom[i].size();
 	}
+	myfile.close();
 
 	for (int i = xMin; i <= xMax; i++) {
 		//cout << hist[i] << " ";
@@ -550,6 +705,7 @@ int main(int argc, char** argv)
 
 	white = find_component();
 	yHorizontal = (yMax + yMin) / 2;
+	xNormal = (xMax + xMin) / 2;
 	drawHist(histogram);
 	cvtColor(white, imgRGB, COLOR_GRAY2RGB);
 
@@ -579,14 +735,16 @@ int main(int argc, char** argv)
 	bool isWhite = imwrite("C:/Users/della_hera/Documents/Hasil Proses/Citra Deteksi Spasi11.jpg", HRWP);
 
 	//posWhiteROI(HRWP);
-	cout << yRegression(yMin, 1) << "Test " << posWhiteTop[xMin].size() << " " << yRegression(yHorizontal, 1) << " " << xRegression(xMin, 15) << " " << xRegression(xMin + 50, 15) << " " << xRegression(xMax, 15) << endl;
+	cout << yRegression(yMin, 1) << "Test " << posWhiteTop[xMin].size() << " " << yRegression(yHorizontal, 1) << " ";// << xRegression(xMin, 15) << " " << xRegression(xMin + 50, 15) << " " << xRegression(xMax, 15) << endl;
 	t1 = clock();
-	deleteWhitePixel(HRWP, 70);
+	equalizeRegression(HRWP);
+	deleteWhitePixel(HRWP, 150);
 	t2 = clock();
 	cout << "\n\nPosition White space = ";
 	cout << "Time " << (float)(t2 - t1) / CLOCKS_PER_SEC << " seconds\n" << endl;
 	adaptiveThreshold(imageContrast, imageBinary, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 7, 2);
 	t1 = clock();
+	imageBinary = clear(HRWP);
 	Mat output = translation(imageBinary);
 	t2 = clock();
 	cout << "\n\nTranslation = ";
